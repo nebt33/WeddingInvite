428328e50b797333405b2c3ccbf28704ce43f41b
nebt33 <nebt33@mst.edu>
2017-07-16 21:06:34 -0500

    Working through learning spring boot
    -Welcome.html probably needs to go away soon.  Landing page should be
    one of the tabs.  Maybe Welcome.html should be spring security page?
    Makes sense.
    
    -Ceremony.html/Reception.html has lots of improvements
    
    -Using some CSS styles from W3Schools.  I should probably download this
    CSS locally into the project.

--- src/main/java/com/example/WeddingInvite/controllers/WeddingInviteController.java
+++ src/main/java/com/example/WeddingInvite/controllers/WeddingInviteController.java
@@ -4,8 +4,9 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class WeddingInviteController {
@@ -17,17 +18,25 @@ public class WeddingInviteController {
 	@Value("${app.welcome.title}")
 	private String TITLE;
 	
-	@RequestMapping(value="/")
+	@GetMapping(value="/")
 	public String welcome(Map<String, Object> model) {
 		model.put("title", TITLE);
 		model.put("message", MESSAGE);
 		return "welcome";
 	}
 	
-	// test 5xx errors
-	@RequestMapping("/5xx")
-	public String ServiceUnavailable() {
-		throw new RuntimeException("ABC");
+	@GetMapping(value="/ceremony")
+	public String ceremony(Map<String, Object> model) {
+		model.put("title", TITLE);
+		model.put("message", MESSAGE);
+		return "ceremony";
+	}
+	
+	@GetMapping(value="/reception")
+	public String reception(Map<String, Object> model) {
+		model.put("title", TITLE);
+		model.put("message", MESSAGE);
+		return "reception";
 	}
 
 }
---
+++ src/main/resources/static/js/navbar.js
@@ -0,0 +1,6 @@
+/**
+ * 
+ */
+//calculating margin-left to align it to center;
+var width = $('.justified').width();
+$('.justified').css('margin-left', '-' + (width / 2)+'px');
